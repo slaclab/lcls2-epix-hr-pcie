778f07a556205f97d46b1e28e3a6dafa20210a85
Benjamin Reese <bareese@slac.stanford.edu>
2022-01-18 14:45:28 -0800

    Set INCLUDE_PGP4_6G env var so that correct XDC file gets included for PGP4 timing

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,6 +1,9 @@
 # Define Firmware Version: v1.2.0.0
 export PRJ_VERSION = 0x01020000
 
+#Use PGP4 Timing
+export INCLUDE_PGP4_6G = 1
+
 # Define release
 ifndef RELEASE
 export RELEASE = lcls2_epix_hr_pcie
